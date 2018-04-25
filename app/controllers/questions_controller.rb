280fd49e75b278fb1328c16f4f4145f6d5c14f7a
Kamil Alkhiary <kalkhiar@uncc.edu>
2018-04-25 17:08:16 +0000

    Questions almost complete but item_id in controller still broken

---
+++ app/controllers/questions_controller.rb
@@ -0,0 +1,19 @@
+class QuestionsController < ApplicationController
+    def create
+        @item = Items.find(params[:item_id])
+        @question = @item.questions.create(question_params)
+        redirect_to item_path(@item)
+    end
+    
+    def destroy
+        @item = Item.find(params[:item_id])
+        @question = @item.questions.find(params[:id])
+        @question.destroy
+        redirect_to items_path(@item)
+    end
+    
+    private
+        def question_params
+            params.require(:question).permit(:name, :question)
+        end
+end
