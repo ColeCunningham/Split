54e1e2bb2ba6b6e6e73529dae39afadf2861dd11
David Visotsky <dvisotsk@uncc.edu>
2018-04-29 18:37:04 +0000

    merge

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -43,7 +43,7 @@ class ItemsController < ApplicationController
     
     private
     def item_params
-       params.require(:item).permit(:title, :description) 
+       params.require(:item).permit(:title, :description, :date) 
     end
     
 end
