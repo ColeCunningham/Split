bf0c6d2f6ec75cb86ccb6080bf250263e9920505
Kamil Alkhiary <kalkhiar@uncc.edu>
2018-04-30 09:51:46 +0000

    Fixed text fields by adding variable next to the label

--- spec/integration/shopper_adds_an_item_spec.rb
+++ spec/integration/shopper_adds_an_item_spec.rb
@@ -1,22 +1,73 @@
 require 'rails_helper.rb'
 
-feature"Shopper adds an article" do
+feature"Shopper adds an item" do
     scenario 'Shopper successfully navigates to the add items page from the homepage' do
         visit items_path
         expect(page).to have_content("List of Items")
         click_link "Add Item"
         expect(page).to have_content("Add Item")
         expect(page).to have_field("Title")
-        expect(page).to have_field("Description")
+        expect(page).to have_field("Image URL")
+        expect(page).to have_field("Total Price")
+        expect(page).to have_field("Quantity available")
+        expect(page).to have_field("Link to site")
     end
     
     scenario "Shopper successfully adds a new item" do
         visit new_item_path
         expect(page).to have_content("Add Item")
         fill_in "Title", with: "New Capybara Item"
-        fill_in "Description", with: "This is a new Capybara Item meant for testing purposes"
+        fill_in "Image URL", with: "www.ImageURL.com"
+        fill_in "Total Price", with: "10.99"
+        fill_in "Quantity available", with: "3"
+        fill_in "Link to site", with: "www.LinkToSite.com"
         click_button "Create Item"
         expect(page).to have_content("New Capybara Item")
-        expect(page).to have_content("This is a new Capybara Item meant for testing purposes")
+        expect(page).to have_content("www.ImageURL.com")
+        expect(page).to have_content("10.99")
+        expect(page).to have_content("3")
+        expect(page).to have_content("www.LinkToSite.com")
     end
+    
+    scenario "Shopper splits a product by signing up to buy it" do
+       visit new_item_path
+        expect(page).to have_content("Add Item")
+        fill_in "Title", with: "New Capybara Item"
+        fill_in "Image URL", with: "www.ImageURL.com"
+        fill_in "Total Price", with: "10.99"
+        fill_in "Quantity available", with: "3"
+        fill_in "Link to site", with: "www.LinkToSite.com"
+        click_button "Create Item"
+        visit items_path
+        expect(page).to have_content("New Capybara Item")
+        expect(page).to have_content("Name")
+        expect(page).to have_content("Quantity")
+        fill_in "Name", with: "Kamil"
+        fill_in "Quantity", with: "3"
+        click_button "Split"
+        expect(page).to have_content("Pending Splits")
+        expect(page).to have_content("Kamil 3")
+    end
+    
+    scenario "Admin edits a product's image after a copyright infringement claim" do
+       visit new_item_path
+        expect(page).to have_content("Add Item")
+        fill_in "Title", with: "New Capybara Item"
+        fill_in "Image URL", with: "www.NFSW-Image.com"
+        fill_in "Total Price", with: "10.99"
+        fill_in "Quantity available", with: "3"
+        fill_in "Link to site", with: "www.LinkToSite.com"
+        click_button "Create Item"
+        visit items_path
+        click_link 'Edit'
+        fill_in "Image URL", with: "www.NFSW-Image-Replaced.com"
+        click_button 'Update Item'
+        expect(page).to have_content("New Capybara Item")
+        expect(page).to have_content("www.NFSW-Image-Replaced.com")
+        expect(page).to have_content("10.99")
+        expect(page).to have_content("3")
+        expect(page).to have_content("www.LinkToSite.com")
+    end
+    
+    
 end
